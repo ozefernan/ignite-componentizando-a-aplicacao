1bcadce9f4de98099ab954adba4e0f3799c9fbcd
Oseas-Fernandes <oseas.fernandes@coxautomotive.com.br>
2021-04-10 14:45:13 -0300

    Challenge Completion.

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,33 @@
-export function SideBar() {
-  // Complete aqui
+import { Button } from "./Button";
+
+interface SidebarProps {
+  selectedGenreId: Number;
+  genres: array;
+  onClickButton: () => void;
+}
+
+export function SideBar({
+  selectedGenreId,
+  genres,
+  onClickButton,
+}: SidebarProps) {
+  return (
+    <nav className="sidebar">
+      <span>
+        Watch<p>Me</p>
+      </span>
+
+      <div className="buttons-container">
+        {genres.map((genre) => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => onClickButton(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  );
 }
